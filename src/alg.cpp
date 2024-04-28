187a2622b685fa946e872fe8c13fbe4cc881ee49
vtlnr <149604564+vtlnr@users.noreply.github.com>
2024-04-28 19:26:27 +0300

    b107

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,32 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int cbinsearch(int* arr, int size, int value){
+  int count = 0;
+  int left = 0;
+  int right = size - 1;
+  while (left <= right){
+      int mid = left + (right - left) / 2;
+      if (arr[mid] == value){
+          count++;
+          int pos = mid - 1;
+          while (pos >= 0 && arr[pos] == value){
+              count++;
+              pos--;
+          }
+          pos = mid + 1;
+          while (pos < size && arr[pos] == value){
+              count++;
+              pos++;
+          }
+          break;
+      }
+      else if (arr[mid] < value){
+          left = mid + 1;
+      }
+      else{
+          right = mid - 1;
+      }
+  }
+  return count;
+	return 0; // если ничего не найдено
 }
